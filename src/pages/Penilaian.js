92260fe97f56190453a17a1a8bc7506e0512fcdd
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2024-10-29 15:30:19 +0700

    Update Penilaian.js

--- src/pages/Penilaian.js
+++ src/pages/Penilaian.js
@@ -2,17 +2,63 @@ import React from 'react';
 
 function Penilaian() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-4">
-      <h1 className="text-4xl font-bold text-darkred mb-4">Selamat Datang di Halaman Beranda!</h1>
-      <p className="text-lg text-gray-700 max-w-lg">
-        Ini adalah halaman beranda sementara untuk aplikasi Anda. Di sini, Anda dapat menambahkan konten utama 
-        atau informasi singkat tentang aplikasi Anda.
-      </p>
-      <p className="text-lg text-gray-700 mt-4">
-        Klik menu di atas untuk menavigasi ke halaman lain.
-      </p>
+    <div className="p-6 bg-gray-100">
+      {/* Header */}
+      <>
+        <p className="p-0 m-0 mt-4" style={{ textAlign: 'left', fontSize: '13pt', fontWeight: 500, color: 'rgb(31, 34, 37)' }}>
+          PENILAIAN MANDIRI
+        </p>
+        <p className="p-0 mb-2" style={{ textAlign: 'left', fontSize: '11pt' }}>
+          Penilaian Mandiri Lembaga Administrasi Negara
+        </p>
+      </>
+
+      {/* Notification Banner */}
+      <div className="bg-teal-700  p-4 rounded-md  text-white p-6 mt-4">
+        Penilaian Mandiri EPSS 2024 sudah berakhir. Terima kasih atas kerjasama dan partisipasi aktif Bapak/Ibu.
+      </div>
+
+      {/* Two-column layout */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+        <div className="bg-white p-4 rounded-md shadow-md">
+          <div className="card" style={{ backgroundColor: 'rgb(249, 250, 252)' }}>
+            <div className="card-body p-1">
+              <p className="text-center p-0 m-0 ps-2">Kegiatan Statistik</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-white p-4 rounded-md shadow-md">
+          <div className="card" style={{ backgroundColor: 'rgb(249, 250, 252)' }}>
+            <div className="card-body p-1">
+              <p className="text-center p-0 m-0 ps-2">Progres Penilaian Mandiri</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      
+
+
+
+
+      {/* Progress Bar */}
+      <div className="card-body">
+        <div className="progress progress-white my-2" style={{ height: '4px' }}>
+          <div
+            className="progress-bar"
+            role="progressbar"
+            aria-valuenow="100"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            style={{ width: '100%' }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Penilaian;
+
