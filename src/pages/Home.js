14502ca1db4dedaf27e232449897ac723a06d2df
pujisubarkah <pujiatmo.subarkah@gmail.com>
2024-10-29 11:13:35 +0700

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-4">
-      <h1 className="text-4xl font-bold text-darkred mb-4">Selamat Datang di Halaman Beranda!</h1>
-      <p className="text-lg text-gray-700 max-w-lg">
-        Ini adalah halaman beranda sementara untuk aplikasi Anda. Di sini, Anda dapat menambahkan konten utama 
-        atau informasi singkat tentang aplikasi Anda.
-      </p>
-      <p className="text-lg text-gray-700 mt-4">
-        Klik menu di atas untuk menavigasi ke halaman lain.
-      </p>
+    <div class="max-w-4xl mx-auto p-6 text-gray-700 leading-relaxed">
+    <h2 class="text-3xl font-bold text-[#23346c] mb-4">
+       Tentang EPSS
+    </h2>
+    <div class="w-16 h-1 bg-[#f3b111] mb-6"></div>
+    <p class="mb-4">
+    Badan Pusat Statistik (BPS) sebagai lembaga statistik nasional menjadi salah satu instansi penghasil data dan informasi statistik di Indonesia, berkomitmen untuk menyediakan statistik yang berkualitas. Di sisi lain BPS terus berupaya mewujudkan dan mengembangkan Sistem Statistik Nasional (SSN) berupa optimalisasi sumber daya dalam penyelenggaraan statistik agar tercipta SSN yang andal, efektif dan efisien. BPS harus meningkatkan perannya dalam aspek koordinasi, integrasi, sinkronisasi dan standardisasi (KISS) dengan seluruh penyelenggara kegiatan statistik, serta perannya sebagai Pembina dalam penyelenggaraan statistik sektoral.
+    </p>
+    <p class="mb-4">
+    institusi dan lembaga penghasil data atau informasi statistik bukan hanya BPS. Unit kerja pemerintahan seperti Kementerian, Lembaga Non Kementerian, Instansi Daerah juga bertanggung jawab terhadap tersedianya statistik sektoral berupa data dan informasi statistik yang berkualitas. Dalam posisinya sebagai Pembina statistik, BPS perlu mengetahui seberapa besar capaian pembangunan dalam bidang perstatistikan nasional seperti diamanatkan dalam UU No 16 Tahun 1997 tentang Statistik dan Perpres 39/2019 tentang Satu Data Indonesia.
+    </p>
+    <p class="mb-4">
+    Pemantauan dan Evaluasi Penyelenggaraan Statistik Sektoral merupakan proses penilaian terhadap pelaksanaan penyelenggaraan statistik sektoral di Instansi Pemerintah Pusat dan Pemerintah Daerah untuk menghasilkan suatu nilai Indeks Pembangunan Statistik (IPS) yang menggambarkan tingkat kematangan (maturity level) dari pelaksanaan penyelenggaraan statistik sektoral di Instansi Pemerintah Pusat dan Pemerintah Daerah.
+    </p>
     </div>
+
   );
 }
 
