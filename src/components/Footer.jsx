65723d3f1487a0043e9ce9713ff384b33a669fa1
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-10-29 14:58:16 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="mt-8">
+            <img src="https://placehold.co/100x100" alt="Logo LANRI" className="inline-block mr-4" />
+            <div className="inline-block align-top">
+                <h2 className="font-bold">Lembaga Administrasi Negara</h2>
+                <p>Pusat Data dan Sistem Informasi</p>
+                <p>Jl. Veteran No 10 Jakarta 11010, Indonesia</p>
+                <p><i className="fas fa-phone"></i> (62-21) xxxxxx</p>
+                <p><i className="fas fa-envelope"></i> pusdatin@lan.go.id</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
