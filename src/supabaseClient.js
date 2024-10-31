58b0e44dca41241fb4629e3fe87eef9713e53198
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-10-31 08:07:28 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,4 +5,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL; // Accessing the Supabase URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY; // Accessing the anon key
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, { db: { schema: 'myschema' } });
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, { db: { schema: 'simbatik' } });
