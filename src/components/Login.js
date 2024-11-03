7e53776ba0c3e7fe8d15d578178e05f42bd0ff66
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-11-03 19:52:49 +0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,53 +36,55 @@ const Login = ({ isOpen, onClose }) => {
     if (!isOpen) return null; // Jika modal tidak terbuka, kembalikan null
 
   return (
-    <div className="relative max-w-sm mx-auto p-4 bg-white shadow-lg rounded-lg flex flex-col">
-      <button onClick={onClose} className="absolute top-[0px] right-[20px] text-5xl text-gray-700 hover:text-gray-900">
-        &times;
-      </button>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="relative max-w-sm mx-auto p-4 bg-white shadow-lg rounded-lg flex flex-col">
+        <button onClick={onClose} className="absolute top-[0px] right-[20px] text-5xl text-gray-700 hover:text-gray-900">
+          &times;
+        </button>
 
-      <div className="flex">
-        {/* Right side - Login Form */}
-        <div className="p-8 w-4/3">
-          <form onSubmit={handleSubmit}>
-            <h2 className="text-2xl font-bold mb-4">Masuk</h2>
+        <div className="flex">
+          {/* Right side - Login Form */}
+          <div className="p-8 w-4/3">
+            <form onSubmit={handleSubmit}>
+              <h2 className="text-2xl font-bold mb-4">Masuk</h2>
 
-            <div className="mb-4">
-              <label className="block text-gray-700">Email</label>
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)} // Update state email
-                required
-                className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700">Password</label>
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)} // Update state password
-                required
-                className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              />
-            </div>
-
-            {error && (
-              <div className="mb-4 text-red-500">
-                {error}
+              <div className="mb-4">
+                <label className="block text-gray-700">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)} // Update state email
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                />
               </div>
-            )}
-            <button
-              type="submit"
-              className="w-full bg-teal-600 text-white py-2 rounded-md hover:bg-teal-800 transition duration-200"
-            >
-              Masuk
-            </button>
-          </form>
+
+              <div className="mb-4">
+                <label className="block text-gray-700">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)} // Update state password
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                />
+              </div>
+
+              {error && (
+                <div className="mb-4 text-red-500">
+                  {error}
+                </div>
+              )}
+              <button
+                type="submit"
+                className="w-full bg-teal-600 text-white py-2 rounded-md hover:bg-teal-800 transition duration-200"
+              >
+                Masuk
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
